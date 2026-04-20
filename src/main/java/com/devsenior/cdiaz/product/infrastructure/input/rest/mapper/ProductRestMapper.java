7f9bb260aa51ffdf5e84b369f28eb93fa2c6c399
Cesar Augusto Diaz Arriaga <cesardevsenior@gmail.com>
2026-04-20 12:44:05 -0500

    Ajuste al Mapper

--- src/main/java/com/devsenior/cdiaz/product/infrastructure/input/rest/mapper/ProductRestMapper.java
+++ src/main/java/com/devsenior/cdiaz/product/infrastructure/input/rest/mapper/ProductRestMapper.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import com.devsenior.cdiaz.product.domain.model.Product;
 import com.devsenior.cdiaz.product.infrastructure.input.rest.dto.ProductRequest;
 import com.devsenior.cdiaz.product.infrastructure.input.rest.dto.ProductResponse;
+import com.devsenior.cdiaz.product.infrastructure.output.jpa.ProductEntity;
 
 @Component
 public class ProductRestMapper {
@@ -16,5 +17,13 @@ public class ProductRestMapper {
     // Transforma el resultado del Dominio a un formato para la API web
     public ProductResponse toDto(Product product) {
         return new ProductResponse(product.getId(), product.getName(), product.getPrice());
+    }
+
+    public ProductEntity toEntity(ProductRequest request) {
+        return new ProductEntity(request.name(), request.price());
+    }
+
+    public Product toDomain(ProductEntity entity) {
+        return new Product(entity.getId(), entity.getName(), entity.getPrice());
     }
 }
